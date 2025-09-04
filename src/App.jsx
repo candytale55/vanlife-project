11dd81538556eb61816f63079a51832df3eec012
CandyTale55 <candytale55@gmail.com>
2025-09-04 19:57:12 +0200

    Create the first 2 routes:  * Home and About.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function Home() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1>I am Home</h1>
   )
 }
 
-export default App
+function About() {
+  return (
+    <h2>I am About</h2>
+  )
+}
+
+/**
+ * Challenge:
+ * Bootstrap the VanLife project by creating the first 2 routes:
+ * Home and About.
+ * 
+ * Also include the navbar that can link between the two routes.
+ * For now, you'll either need to copy/paste the navbar code
+ * to both Home and About pages, or you'll need to find a place
+ * to put it where it can be shared between the two pages.
+ * (Don't overthink this part - just do whatever is easiest for
+ * you because we'll learn a better approach very soon)
+ * 
+ * Review challenge: do all the CSS yourself based on the design
+ * linked in the slides.
+ */
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
