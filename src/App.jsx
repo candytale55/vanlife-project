6f0ea40ddbbb7b23e8982d9b6c11eef1af4153dc
CandyTale55 <candytale55@gmail.com>
2025-09-08 14:56:45 +0200

    fix nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -32,21 +32,21 @@ export default function App() {
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-        </Route>
+        
 
         <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
+        
+          <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+          </Route>  
+          </Route>
         </Route>
-
-        <Route path="vans/:id" element={<HostVanDetail />}>
-            <Route index element={<HostVanInfo />} />
-            <Route path="pricing" element={<HostVanPricing />} />
-            <Route path="photos" element={<HostVanPhotos />} />
-        </Route>  
-
       </Routes>
       </BrowserRouter>
   );
