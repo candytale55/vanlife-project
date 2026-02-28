cf4545060342c0c1847d0dcdfe30ecaf79cbb03d
CandyTale55 <candytale55@gmail.com>
2026-02-28 15:24:49 +0100

    refactor(VanDetail): clean up code by removing unused imports and comments, and make fetching logic work (no Links)

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,52 +1,50 @@
-import React from "react"
-import { Link, useParams, useLocation } from "react-router-dom"
+import { Link, useParams, } from "react-router-dom"
 import { useEffect, useState } from "react"
 
-// TODO: Implement a custom hook for fetching APIs with loading and error
 
-// ===== VAN DETAIL COMPONENT =====
 export default function VanDetail() {
-    // ===== ROUTE PARAMS & STATE =====
-    const params = useParams()
-    /*     console.log("Params (van #): ", params); // TODO: Remove once testing is done. */
-    const [van, setVan] = useState(null);
-    /* const location = useLocation() */
 
+    const params = useParams()
+    const [van, setVan] = useState(null);
+    console.log("Van#: ", van); // TODO: Remove once tests are done. 
     
     // Fetch individual van data based on ID from URL
-
+    //TODO: Implement a useFetch custom Hook for this
     useEffect(() => {
         async function fetchData() {
+
             try {
                 const response = await fetch(`/api/vans/${params.id}`);
 
                 const cleanedData = await response.json();
 
-                setVan(cleanedData);
-
-                console.log(van); // TODO: Remove once testing is done.
-
+                setVan(cleanedData.van);
                 
             } catch (error) {
-                console.log(error.message); // TODO: Remove once you implement the useFetch custom hook with loading and error
-
-            }
+                console.log(error.message); // TODO: Remove once testing is done.
+            } 
         }
         fetchData();
     }, [params.id]);
     
-    
-    /* React.useEffect(() => {
-        fetch(`/api/vans/${params.id}`)
-            .then(res => res.json())
-            .then(data => setVan(data.vans))
-    }, [params.id])
- */
+
     /* const search = location.state?.search || "";
     const type = location.state?.type || "all"; */
 
     return (
         <div className="van-detail-container">
+            <h2>I am VanDetail</h2>
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+            
             {/* <Link
                 to={`..${search}`}
                 relative="path"
@@ -65,7 +63,7 @@ export default function VanDetail() {
                     <button className="link-button">Rent this van</button>
                 </div>
             ) : <h2>Loading...</h2>} */}
-            <h2>I am VanDetail</h2>
+            
         </div>
     )
 }
